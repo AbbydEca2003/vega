3356726c73d1133b1b78471ffbdf54b1e3f0d892
Arnav Shrestha <arnavshrestha.as@gmail.com>
2024-08-12 16:26:08 +0545

    test67

--- resources/views/frontend/welcome.blade.php
+++ resources/views/frontend/welcome.blade.php
@@ -45,7 +45,7 @@
                 <a class="btn btn-primary btn-xl text-uppercase" href="#about_us">Tell Me More</a>
             </div>
         </header>
-       Hello from arnav
+       Hello from arnav everyone
         
         @foreach($page as $page)
         @if($page->status === '1')
